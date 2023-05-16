f8898c7af56c534a9ff574fc27afad0e7eb5dd2e
sukka <isukkaw@gmail.com>
2023-05-16 19:16:04 +0800

    Remove redudant output

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,6 @@ launch() {
     SNELL_PSK=`hexdump -n 16 -e '4/4 "%08x" 1 "\n"' /dev/urandom`
   fi
 
-  echo "snell psk: ${SNELL_PSK}"
-  echo "snell listen: ${SNELL_HOST}:${SNELL_PORT}"
-  echo "snell obfs: ${SNELL_OBFS}"
-
   cat > snell.conf <<EOF
 [snell-server]
 listen = ${SNELL_HOST}:${SNELL_PORT}
@@ -19,7 +15,7 @@ EOF
   cat snell.conf
   snell-server -c snell.conf &
 
-  (echo "$VPN_PASSWORD"; echo "${VPN_AUTH_CODE}") | openconnect --user="${VPN_USER}" --passwd-on-stdin --servercert="${VPN_CERT_SHA256}" --authgroup="${VPN_AUTH_GROUP}" --os=linux-64 --passwd-on-stdin --no-dtls "${VPN_HOST}"
+  (echo "$VPN_PASSWORD"; echo "${VPN_AUTH_CODE}") | openconnect --user="${VPN_USER}" --passwd-on-stdin --servercert="${VPN_CERT_SHA256}" --authgroup="${VPN_AUTH_GROUP}" --os=linux-64 "${VPN_HOST}"
 }
 
 if [ -z "$@" ]; then
